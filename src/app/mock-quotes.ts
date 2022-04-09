44317aa6af0e84aabbed89d176ab67dafd8a7886
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-09 22:14:57 +0300

    ft: create mock quotes for displaying

---
+++ src/app/mock-quotes.ts
@@ -0,0 +1,22 @@
+import { Quote } from './quote';
+
+export const QUOTES: Quote[] = [
+  {
+    id: 1,
+    text: 'it takes as much energy to wish as it is to plan',
+    author: 'Eleanor Roosevelt',
+    date: '1947-06-12',
+  },
+  {
+    id: 2,
+    text: 'Never overlook the power of simplicity.',
+    author: 'Robin S. Sharma',
+    date: '2022-04-07',
+  },
+  {
+    id: 3,
+    text: 'We deem those happy who from the experience of life have learned to bear its ills without being overcome by them.',
+    author: 'Juvenal',
+    date: '2020-04-01',
+  },
+];
