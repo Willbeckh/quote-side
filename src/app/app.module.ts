b1d05ade80b59add2426045b4e9396fd4707f02f
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-09 22:20:39 +0300

    ch: update applications modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CreateQuoteComponent } from './components/create-quote/create-quote.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { QuoteItemComponent } from './components/quote-item/quote-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    CreateQuoteComponent,
+    QuotesComponent,
+    QuoteItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
