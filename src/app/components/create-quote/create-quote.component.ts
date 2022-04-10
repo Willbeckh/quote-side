9168be147f50ef6323520c7c3b78f2a5bd6169da
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-10 13:48:58 +0300

    ft: create-quote-- update form for creating quotes

--- src/app/components/create-quote/create-quote.component.ts
+++ src/app/components/create-quote/create-quote.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../../quote';
 @Component({
   selector: 'app-create-quote',
   templateUrl: './create-quote.component.html',
-  styleUrls: ['./create-quote.component.css']
+  styleUrls: ['./create-quote.component.css'],
 })
 export class CreateQuoteComponent implements OnInit {
+  @Output() onAddQuote: EventEmitter<Quote> = new EventEmitter();
+  text!: string;
+  author!: string;
+  date!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    // validate input
+    if (!this.text) {
+      alert('please enter a quote');
+      return;
+    }
+
+    const newQuote = {
+      text: this.text,
+      author: this.author,
+      date: this.date,
+    };
+
+    // @TODO: send quote to server via emit
+    this.onAddQuote.emit(newQuote);
+
+    // reset form
+    this.text = '';
+    this.author = '';
   }
 
+  
 }
