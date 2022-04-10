fcdb0819c81042308987db5d3aaa12118266ef64
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-10 13:25:19 +0300

    ch: create- update routing for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
 import { CreateQuoteComponent } from './components/create-quote/create-quote.component';
 import { QuotesComponent } from './components/quotes/quotes.component';
 import { QuoteItemComponent } from './components/quote-item/quote-item.component';
+import { HomeComponent } from './components/home/home.component';
+import { LikeBoxComponent } from './components/like-box/like-box.component';
+import { DislikeBoxComponent } from './components/dislike-box/dislike-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent,
     CreateQuoteComponent,
     QuotesComponent,
     QuoteItemComponent,
+    HomeComponent,
+    LikeBoxComponent,
+    DislikeBoxComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
