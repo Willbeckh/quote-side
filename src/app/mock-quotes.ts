83de28d5362e917fb1cb952fbd43f95c8425e6fa
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-12 15:13:29 +0300

    ft; update quote object interface

--- src/app/mock-quotes.ts
+++ src/app/mock-quotes.ts
@@ -1,22 +1,25 @@
 import { Quote } from './quote';
 
 export const QUOTES: Quote[] = [
-  // {
-  //   id: 1,
-  //   text: 'it takes as much energy to wish as it is to plan',
-  //   author: 'Eleanor Roosevelt',
-  //   date: '1947-06-12',
-  // },
-  // {
-  //   id: 2,
-  //   text: 'Never overlook the power of simplicity.',
-  //   author: 'Robin S. Sharma',
-  //   date: '2022-04-07',
-  // },
-  // {
-  //   id: 3,
-  //   text: 'We deem those happy who from the experience of life have learned to bear its ills without being overcome by them.',
-  //   author: 'Juvenal',
-  //   date: '2020-04-01',
-  // },
+  {
+    id: 1,
+    text: 'it takes as much energy to wish as it is to plan',
+    author: 'Eleanor Roosevelt',
+    upvotes: 100,
+    downvotes: 2,
+  },
+  {
+    id: 2,
+    text: 'Never overlook the power of simplicity.',
+    author: 'Robin S. Sharma',
+    upvotes: 24,
+    downvotes: 1,
+  },
+  {
+    id: 3,
+    text: 'Opportunity does not knock, it presents itself when you beat down the door.',
+    author: 'Kyle Chandler',
+    upvotes: 10,
+    downvotes: 0,
+  },
 ];
