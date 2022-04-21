1c2f6f646c9ef5283b0fa67a618001fbbe911bca
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-21 20:59:15 +0300

    Auto-generated commit [ci skip]

--- README.md
+++
@@ -1,2 +0,0 @@
-# QuoteSide
-s
--- src/app/quote.ts
+++
@@ -1,8 +0,0 @@
-export interface Quote {
-  // this is the interface for the quote object
-  id?: number;
-  text: string;
-  author: string;
-  upvotes: number;
-  downvotes: number;
-}
